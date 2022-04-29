58f56ec165c9a2dc9453e2c87d0ea268997678a0
sumit <studyzone.sumit@gmail.com>
2022-04-29 22:18:51 +0530

    change point setting and rename functions

--- app.js
+++ app.js
@@ -2,64 +2,65 @@
 let st_btn = document.getElementById('start');
 let stp_btn = document.getElementById('stop');
 let rst_btn = document.getElementById('reset');
-let sec = document.getElementById('second');
+let pt = document.getElementById('points');
+let sec = document.getElementById('seconds');
 let min = document.getElementById('minute');
-let hour = document.getElementById('hour');
 
-let sec_val = 0, min_val=0, hour_val=0;
+let pt_val = 0, sec_val=55, min_val=59;
 
 
 let id = null;
 // reset function
 function reset(){
-    stop();
+  stop();
+  pt_val = 0;
   sec_val = 0;
   min_val = 0;
-  hour_val = 0;
 
-  sec.innerText = '00';
+  pt.innerText = '00';
+  sec.innerText = '00 : ';
   min.innerText = '00 : ';
-  hour.innerText = '00 : ';
 
+}
+// start functionality
+function increse_min(){
+    min_val += 1;
+    if(min_val > 59){
+      min_val = 0;
+    min.innerText = '00 : ';
+    }else if(min_val > 9){
+        min.innerText = `${min_val} : `;
+    }else{
+        min.innerText = `0${min_val} : ` ;
+    }
 }
 
 
-function increse_minute(){
-    min_val += 1;
-   if(min_val > 59){
-     increse_hour();
-     min_val = 0;
-     min.innerText = '00 : ';
-   }else if(min_val > 9){
-       min.innerText = `${min_val} : `;
+function increse_sec(){
+    sec_val += 1;
+   if(sec_val > 59){
+     increse_min();
+     sec_val = 0;
+     sec.innerText = '00 : ';
+   }else if(sec_val > 9){
+       sec.innerText = `${sec_val} : `;
    }else{
-       min.innerText = `0${min_val} : ` ;
+       sec.innerText = `0${sec_val} : ` ;
    }
 }
 
-function increse_hour(){
-    hour_val += 1;
-    if(hour_val > 59){
-      increse_hour();
-      hour_val = 0;
-      min.innerText = '00 : ';
-    }else if(hour_val > 9){
-        hour.innerText = `${min_val} : `;
-    }else{
-        hour.innerText = `0${hour_val} : ` ;
-    }
-}
-function update_time(){
-   sec_val += 1;
-   if(sec_val > 59){
-       increse_minute();
-       sec_val = 0;
-       sec.innerText = '00';
 
-   }else if(sec_val > 9){
-       sec.innerText = sec_val;
+function update_time(){
+   pt_val += 1;
+   if(pt_val > 99){
+       increse_sec();
+       pt_val = 0;
+       pt.innerText = '00';
+
+   }else if(pt_val > 9){
+       pt.innerText = pt_val;
    }else{
-       sec.innerText = `0${sec_val}`;
+       pt.innerText = `0${pt_val}`;
    }
 }
 function start(){
@@ -69,6 +70,8 @@ if(id != null){
 }
 id = setInterval(update_time, 10);
 }
+
+// stop function
 
 function stop(){
    clearInterval(id);
